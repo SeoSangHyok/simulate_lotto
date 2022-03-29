a4cc7e637290e462da2b957613972027daf9ae67
SeoSangHyok <31233650+SeoSangHyok@users.noreply.github.com>
2022-03-29 09:54:36 +0000

    해야 할일 1번 정의

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ print("hello world!!")
 j = requests.get("https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=1008")
 
 print(j.json())
+
+# 해야할일
+# 1. 로또 데이터 베이스에서 1회차~가장 최근꺼 까지 request로 받아온 후 csv에 저장(업데이트)
