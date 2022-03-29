19d03c1d35b24aa0973abc23c7990eaa4fa3d751
Seo Sang Hyok <peace-day@hanmail.net>
2022-03-29 21:46:50 +0900

    로또 DB에서 긁어와서  데이터 프레임으로 처리하는것 처리

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 import requests
-
-
-print("hello world!!")
-j = requests.get("https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=1008")
-
-print(j.json())
+import pandas
 
 # 해야할일
 # 1. 로또 데이터 베이스에서 1회차~가장 최근꺼 까지 request로 받아온 후 csv에 저장(업데이트)
+
+lotto_history_df = {
+    "drwNo": [],
+    "drwNoDate": [],
+    "totSellamnt": [],
+    "firstWinamnt": [],
+    "firstPrzwnerCo": [],
+    "drwtNo1": [],
+    "drwtNo2": [],
+    "drwtNo3": [],
+    "drwtNo4": [],
+    "drwtNo5": [],    
+    "drwtNo6": [],    
+    "bnusNo": [],            
+}
+
+
+for drwNo in range (1,5):
+    req_url = f"https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={drwNo}"
+    req_result = requests.get(req_url)
+    result_json = req_result.json()
+    if result_json["returnValue"] == "success":
+        for key, value in result_json.items():
+            if key in lotto_history_df.keys():
+                lotto_history_df[key].append(value)
+
+df = pandas.DataFrame()
+df = df.from_dict(lotto_history_df)
+print(df)
